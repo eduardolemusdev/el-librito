2f57141bd25dc190193381856ec3dae1153a8156
Eduardo Lemus <eduardolemus3098@gmail.com>
2024-12-01 21:23:48 -0600

    se agrego las portadas de los libros

--- src/app/features/services/data.ts
+++ src/app/features/services/data.ts
@@ -1,220 +1,258 @@
-export const books = [{
-    id: "1",
-    name: "Cien Años de Soledad",
+export const books = [
+  {
+    id: '1',
+    name: 'Cien Años de Soledad',
     price: 19.99,
     quantity: 10,
-    description: "Una novela emblemática de Gabriel García Márquez sobre la familia Buendía en el pueblo ficticio de Macondo.",
+    description:
+      'Una novela emblemática de Gabriel García Márquez sobre la familia Buendía en el pueblo ficticio de Macondo.',
     starsNumber: 5,
     discount: 10,
-    category: "Ficción",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "2",
-    name: "Don Quijote de la Mancha",
+    category: 'Ficción',
+    coverUrl: 'https://www.mrbooks.com/mrbooks/portadas/9789585118010.webp',
+  },
+  {
+    id: '2',
+    name: 'Don Quijote de la Mancha',
     price: 14.99,
     quantity: 7,
-    description: "La clásica novela de Miguel de Cervantes sobre las aventuras de un caballero y su fiel escudero.",
+    description:
+      'La clásica novela de Miguel de Cervantes sobre las aventuras de un caballero y su fiel escudero.',
     starsNumber: 5,
     discount: 15,
-    category: "Clásicos",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "3",
-    name: "1984",
+    category: 'Clásicos',
+    coverUrl:
+      'https://m.media-amazon.com/images/I/71kdJikvR4L._AC_UF1000,1000_QL80_.jpg',
+  },
+  {
+    id: '3',
+    name: '1984',
     price: 12.99,
     quantity: 5,
-    description: "La famosa novela distópica de George Orwell que examina la vigilancia y el control totalitario.",
+    description:
+      'La famosa novela distópica de George Orwell que examina la vigilancia y el control totalitario.',
     starsNumber: 4,
     discount: 5,
-    category: "Distopía",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "4",
-    name: "Fahrenheit 451",
+    category: 'Distopía',
+    coverUrl:
+      'https://m.media-amazon.com/images/I/612ADI+BVlL._AC_UF1000,1000_QL80_.jpg',
+  },
+  {
+    id: '4',
+    name: 'Fahrenheit 451',
     price: 11.99,
     quantity: 8,
-    description: "Un futuro donde los libros son quemados y el pensamiento independiente es perseguido, de Ray Bradbury.",
+    description:
+      'Un futuro donde los libros son quemados y el pensamiento independiente es perseguido, de Ray Bradbury.',
     starsNumber: 4,
     discount: 20,
-    category: "Ciencia Ficción",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "5",
-    name: "El Gran Gatsby",
+    category: 'Ciencia Ficción',
+    coverUrl:
+      'https://m.media-amazon.com/images/I/61wtqek8ONL._AC_UF1000,1000_QL80_.jpg',
+  },
+  {
+    id: '5',
+    name: 'El Gran Gatsby',
     price: 13.99,
     quantity: 6,
-    description: "Una reflexión sobre el sueño americano y la decadencia social, escrita por F. Scott Fitzgerald.",
+    description:
+      'Una reflexión sobre el sueño americano y la decadencia social, escrita por F. Scott Fitzgerald.',
     starsNumber: 4,
     discount: 10,
-    category: "Clásicos",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "6",
-    name: "Crimen y Castigo",
+    category: 'Clásicos',
+    coverUrl: 'https://www.mrbooks.com/mrbooks/portadas/9788418395185.webp',
+  },
+  {
+    id: '6',
+    name: 'Crimen y Castigo',
     price: 16.99,
     quantity: 9,
-    description: "La obra maestra de Dostoyevski sobre la moral, el sufrimiento y la redención.",
+    description:
+      'La obra maestra de Dostoyevski sobre la moral, el sufrimiento y la redención.',
     starsNumber: 5,
     discount: 12,
-    category: "Ficción Clásica",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "7",
-    name: "La Sombra del Viento",
+    category: 'Ficción Clásica',
+    coverUrl:
+      'https://www.catedra.com/imagenes/libros/grande/9788437614038-crimen-y-castigo.jpg',
+  },
+  {
+    id: '7',
+    name: 'La Sombra del Viento',
     price: 18.99,
     quantity: 3,
-    description: "Una novela de misterio y amor en la Barcelona de la posguerra, escrita por Carlos Ruiz Zafón.",
+    description:
+      'Una novela de misterio y amor en la Barcelona de la posguerra, escrita por Carlos Ruiz Zafón.',
     starsNumber: 4,
     discount: 18,
-    category: "Misterio",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "8",
-    name: "El Alquimista",
+    category: 'Misterio',
+    coverUrl:
+      'https://internacionallibrosyregalos.com/cdn/shop/products/9788408163435_552x.jpg?v=1597265402',
+  },
+  {
+    id: '8',
+    name: 'El Alquimista',
     price: 9.99,
     quantity: 20,
-    description: "Una fábula moderna de Paulo Coelho sobre la búsqueda del sueño personal.",
+    description:
+      'Una fábula moderna de Paulo Coelho sobre la búsqueda del sueño personal.',
     starsNumber: 5,
     discount: 25,
-    category: "Ficción Filosófica",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "9",
-    name: "Harry Potter y la Piedra Filosofal",
+    category: 'Ficción Filosófica',
+    coverUrl:
+      'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/263/original/portada_el-alquimista_barry-kitson_202404031526.jpg',
+  },
+  {
+    id: '9',
+    name: 'Harry Potter y la Piedra Filosofal',
     price: 14.99,
     quantity: 15,
-    description: "El inicio de la famosa saga de J.K. Rowling sobre un joven mago y su lucha contra el mal.",
+    description:
+      'El inicio de la famosa saga de J.K. Rowling sobre un joven mago y su lucha contra el mal.',
     starsNumber: 5,
     discount: 10,
-    category: "Fantasía",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "10",
-    name: "Orgullo y Prejuicio",
+    category: 'Fantasía',
+    coverUrl:
+      'https://static.wikia.nocookie.net/esharrypotter/images/9/9a/Harry_Potter_y_la_Piedra_Filosofal_Portada_Espa%C3%B1ol.PNG',
+  },
+  {
+    id: '10',
+    name: 'Orgullo y Prejuicio',
     price: 10.99,
     quantity: 4,
-    description: "La obra de Jane Austen que explora el amor, la clase y el matrimonio en la Inglaterra del siglo XIX.",
+    description:
+      'La obra de Jane Austen que explora el amor, la clase y el matrimonio en la Inglaterra del siglo XIX.',
     starsNumber: 5,
     discount: 15,
-    category: "Romántico",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "11",
-    name: "El Nombre del Viento",
+    category: 'Romántico',
+    coverUrl:
+      'https://internacionallibrosyregalos.com/cdn/shop/products/ORGULLOYPREJUICIO_d8838bbb-a901-4956-aaee-dac18ff2bc4f_341x.jpg?v=1641419436',
+  },
+  {
+    id: '11',
+    name: 'El Nombre del Viento',
     price: 16.49,
     quantity: 12,
-    description: "La historia épica de Kvothe, un hombre que llega a ser una leyenda, escrita por Patrick Rothfuss.",
+    description:
+      'La historia épica de Kvothe, un hombre que llega a ser una leyenda, escrita por Patrick Rothfuss.',
     starsNumber: 5,
     discount: 8,
-    category: "Fantasía Épica",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "12",
-    name: "La Casa de los Espíritus",
+    category: 'Fantasía Épica',
+    coverUrl:
+      'https://lapiedradesisifo.com/wp-content/uploads/2016/09/libro_13219293042.jpg',
+  },
+  {
+    id: '12',
+    name: 'La Casa de los Espíritus',
     price: 17.99,
     quantity: 6,
-    description: "Una historia familiar cargada de realismo mágico, escrita por Isabel Allende.",
+    description:
+      'Una historia familiar cargada de realismo mágico, escrita por Isabel Allende.',
     starsNumber: 4,
     discount: 12,
-    category: "Realismo Mágico",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "13",
-    name: "Los Miserables",
+    category: 'Realismo Mágico',
+    coverUrl:
+      'https://internacionallibrosyregalos.com/cdn/shop/products/9788466362085_552x.jpg?v=1672245229',
+  },
+  {
+    id: '13',
+    name: 'Los Miserables',
     price: 21.99,
     quantity: 2,
-    description: "El clásico de Victor Hugo que examina la justicia, el amor y la redención en la Francia post-revolucionaria.",
+    description:
+      'El clásico de Victor Hugo que examina la justicia, el amor y la redención en la Francia post-revolucionaria.',
     starsNumber: 5,
     discount: 7,
-    category: "Clásicos",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "14",
-    name: "El Perfume",
-    price: 12.50,
+    category: 'Clásicos',
+    coverUrl:
+      'https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg',
+  },
+  {
+    id: '14',
+    name: 'El Perfume',
+    price: 12.5,
     quantity: 8,
-    description: "Una novela de Patrick Süskind que narra la vida de un hombre con un sentido del olfato excepcional.",
+    description:
+      'Una novela de Patrick Süskind que narra la vida de un hombre con un sentido del olfato excepcional.',
     starsNumber: 4,
     discount: 18,
-    category: "Misterio",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "15",
-    name: "Matar a un Ruiseñor",
+    category: 'Misterio',
+    coverUrl:
+      'https://cdn.kobo.com/book-images/9cd6ff8c-ae73-4c8b-b161-326c1887d48c/1200/1200/False/les-miserables-291.jpg',
+  },
+  {
+    id: '15',
+    name: 'Matar a un Ruiseñor',
     price: 13.49,
     quantity: 9,
-    description: "Una novela de Harper Lee sobre la injusticia racial en el sur de Estados Unidos.",
+    description:
+      'Una novela de Harper Lee sobre la injusticia racial en el sur de Estados Unidos.',
     starsNumber: 5,
     discount: 20,
-    category: "Drama",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "16",
-    name: "El Hobbit",
+    category: 'Drama',
+    coverUrl:
+      'https://covers.storytel.com/jpg-640/9780718076825.9c45ac8a-3bca-4b83-a6ad-1c0974464b4d?optimize=high',
+  },
+  {
+    id: '16',
+    name: 'El Hobbit',
     price: 15.99,
     quantity: 5,
-    description: "La clásica novela de fantasía de J.R.R. Tolkien sobre las aventuras de Bilbo Bolsón.",
+    description:
+      'La clásica novela de fantasía de J.R.R. Tolkien sobre las aventuras de Bilbo Bolsón.',
     starsNumber: 5,
     discount: 5,
-    category: "Fantasía",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "17",
-    name: "Cumbres Borrascosas",
+    category: 'Fantasía',
+    coverUrl:
+      'https://proassetspdlcom.cdnstatics2.com/usuaris/libros/fotos/357/original/portada_el-hobbit_j-r-r-tolkien_202207271130.jpg',
+  },
+  {
+    id: '17',
+    name: 'Cumbres Borrascosas',
     price: 11.99,
     quantity: 10,
-    description: "El intenso relato de amor y venganza entre Heathcliff y Catherine, escrito por Emily Brontë.",
+    description:
+      'El intenso relato de amor y venganza entre Heathcliff y Catherine, escrito por Emily Brontë.',
     starsNumber: 4,
     discount: 10,
-    category: "Romántico",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "18",
-    name: "La Metamorfosis",
+    category: 'Romántico',
+    coverUrl:
+      'https://internacionallibrosyregalos.com/cdn/shop/products/CUMBRESBORRASCOSAS_238x.jpg?v=1586223122',
+  },
+  {
+    id: '18',
+    name: 'La Metamorfosis',
     price: 8.99,
     quantity: 14,
-    description: "La famosa obra de Franz Kafka sobre un hombre que se convierte en un insecto gigante.",
+    description:
+      'La famosa obra de Franz Kafka sobre un hombre que se convierte en un insecto gigante.',
     starsNumber: 5,
     discount: 10,
-    category: "Ficción Clásica",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "19",
-    name: "El Código Da Vinci",
+    category: 'Ficción Clásica',
+    coverUrl: 'https://www.udllibros.com/imagenes/9788496/978849681502.JPG',
+  },
+  {
+    id: '19',
+    name: 'El Código Da Vinci',
     price: 16.99,
     quantity: 6,
-    description: "Una novela de misterio de Dan Brown que sigue la búsqueda de un secreto oculto en la historia del cristianismo.",
+    description:
+      'Una novela de misterio de Dan Brown que sigue la búsqueda de un secreto oculto en la historia del cristianismo.',
     starsNumber: 4,
     discount: 12,
-    category: "Misterio",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-},
-{
-    id: "20",
-    name: "Los Juegos del Hambre",
+    category: 'Misterio',
+    coverUrl:
+      'https://internacionallibrosyregalos.com/cdn/shop/files/9788408273790_1_785x.jpg?v=1696874135',
+  },
+  {
+    id: '20',
+    name: 'Los Juegos del Hambre',
     price: 13.99,
     quantity: 11,
-    description: "Una historia distópica de Suzanne Collins sobre una sociedad en la que los jóvenes deben luchar hasta la muerte.",
+    description:
+      'Una historia distópica de Suzanne Collins sobre una sociedad en la que los jóvenes deben luchar hasta la muerte.',
     starsNumber: 5,
     discount: 20,
-    category: "Distopía",
-    coverUrl: "https://m.media-amazon.com/images/I/71P+4DslKmL._AC_UF1000,1000_QL80_.jpg"
-}];
+    category: 'Distopía',
+    coverUrl: 'https://m.media-amazon.com/images/I/71un2hI4mcL.jpg',
+  },
+];
